5c02c11c73e2cc5d4ab0188993282c3ac6da2b5a
Sundara Raghav.G <g.sundararaghav2005@gmail.com>
2024-08-12 09:10:30 +0530

    Add files via upload

--- node js/express js/express.js
+++ node js/express js/express.js
@@ -1,34 +1,19 @@
 const express=require('express');
 const fs=require('fs')
-// step2
 const app=express()
-
-
-//reading the json from the file
 const readjsondata=()=>{
-   const jsondata=fs.readFileSync('db.json');  //
+   const jsondata=fs.readFileSync('db.json');  
    return JSON.parse(jsondata)
 }
-
-
-
-
-app.get('/',(req,res)=>{
-   res.send("<h1>homepage</h1>")
+app.get('/',(req,res)=>{   res.send("<h1>This is a home page</h1>")
 })
-
-
 app.get('/about',(req,res)=>{
-   res.send("THIS IS ABOUT PAGE")
+   res.send("ABOUT PAGE")
 })
-
-
 app.get('/user',(req,res)=>{
    const name=req.query.q;
    res.send("welcome "+name)
 })
-
-
 app.get('/userage',(req,res)=>{
    const age=req.query.age;
    if(age>=18 && age<100)
@@ -38,35 +23,18 @@ app.get('/userage',(req,res)=>{
    }
    else if(age>=100)
    {
-       res.send("Its impossible to predict")
-
-
+       res.send("impossible to predict")
    }
    else{
       
        console.log("you are not eligible to drive")
        res.send("you are not eligible to drive")
    }
-   // res.send("Age: "+age)
 })
-
-
 app.get('/data',(req,res)=>{
        const dt=readjsondata();
        res.send(dt)
 })
-
-
-
-
-
-
 app.listen(3000,()=>{
    console.log("server is runnning");
 })
-
-
-
-
-
-
---
+++ practice/Html_css/Scripts.js
@@ -0,0 +1,6 @@
+function redirectToLinkedIn() {
+    window.location.href = 'https://www.linkedin.com/in/sundara-raghav-g-113110292/';
+}
+
+
+
